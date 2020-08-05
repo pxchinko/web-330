77142acf537e306fe2ba2d289ff2b395b76698ba
Sarah Massie <pxchinko@gmail.com>
2020-08-05 16:22:45 -0700

    master

---
+++ week-1/Massie-Exercise-1-3.js
@@ -0,0 +1,25 @@
+var header = require('../header.js');
+
+/*
+ Expected output:
+
+ FirstName LastName
+ <AssignmentName>
+ <Today's Date>
+
+ -- DISPLAYING CELL PHONE DETAILS --
+ Manufacturer: <manufacturer>
+ Model: <model>
+ Color: <color>
+ Price: <price>
+
+
+*/
+
+// start program
+
+
+
+
+
+// end program
