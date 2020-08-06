f3a2048ea6eb8e4f047595bc462de13f0bf97f3e
Sarah Massie <pxchinko@gmail.com>
2020-08-05 17:04:49 -0700

    master

--- week-1/Massie-Exercise-1-3.js
+++ week-1/Massie-Exercise-1-3.js
@@ -1,5 +1,5 @@
-var header = require('../header.js');
-
+const header = require('../Massie-header.js');
+  console.log(header.display("Sarah", "Massie", "Exercise 1.3"));
 /*
  Expected output:
 
@@ -12,14 +12,29 @@ var header = require('../header.js');
  Model: <model>
  Color: <color>
  Price: <price>
-
-
 */
 
 // start program
 
-
-
-
+function Cellphone(manufacturer, model, color, price) {
+  this.manufacturer = manufacturer;
+  this.model = model;   
+  this.color = color;
+  this.price = price;
+  // get price function
+  this.getPrice = function() {
+    return this.price;
+  };
+  // get model function
+  this.getModel = function() {
+    return this.model;
+  };
+  // get details function
+  this.getDetails = function() {
+    return "Manufacturer: " + this.manufacturer + "\nModel: " + this.getModel() + "\nColor: " + this.color + "\nPrice: " + this.getPrice();
+  };
+};
+var cellphone = new Cellphone("Motorola", "RAZR V3m", "Pink", "$50");    
+console.log("\n" + cellphone.getDetails())
 
 // end program
